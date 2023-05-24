cedb546f5e18f82494c5817ca43951b0218806ae
Tony Wu <tonywu3027@me.com>
2023-05-24 02:49:00 +0100

    Initialise project with start code

---
+++ src/main/java/uk/ac/ic/doc/blocc/Main.java
@@ -0,0 +1,13 @@
+package uk.ac.ic.doc.blocc;
+
+/** Starter code for the project. */
+public class Main {
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
